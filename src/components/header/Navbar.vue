4ed86a45ff6ea36701af794ccee94d3ff52008d7
gluke0 <milanesigianluca@gmail.com>
2023-05-04 16:53:07 +0200

    navbar quasi finita

--- src/components/header/Navbar.vue
+++ src/components/header/Navbar.vue
@@ -7,16 +7,43 @@ export default {
                 {
                     menu: 'Characters',
                     active: false,
-                }
-                'Comics',
-                'Movies',
-                'TV',
-                'Games',
-                'Collectibles',
-                'Videos',
-                'Fans',
-                'News',
-                'Shop'
+                },
+                {
+                    menu: 'Comics',
+                    active: true,
+                },
+                {
+                    menu: 'Movies',
+                    active: false,
+                },
+                {
+                    menu: 'TV',
+                    active: false,
+                },
+                {
+                    menu: 'Games',
+                    active: false,
+                },
+                {
+                    menu: 'Collectibles',
+                    active: false,
+                },
+                {
+                    menu: 'Videos',
+                    active: false,
+                },
+                {
+                    menu: 'Fans',
+                    active: false,
+                },
+                {
+                    menu: 'News',
+                    active: false,
+                },
+                {
+                    menu: 'Shop',
+                    active: false,
+                },
             ]
         }
     }
@@ -26,7 +53,7 @@ export default {
 <template>
     <div id="navbar">
         <ul>
-            <li v-for="item in navbar"><a href="#">{{ item }}</a></li>
+            <li v-for="item in navbar"><a href="#" :class="{active: item.active == true}">{{ item.menu }}</a></li>
         </ul>
     </div>
 </template>
@@ -37,13 +64,17 @@ export default {
 ul {
     display: flex;
     text-transform: uppercase;
+    align-items: center;
 
     li{
-        margin-left: 1.25rem;
+        margin-left: 1.80rem;
 
         a{
             color: variables.$darken-color;
-            &.active {
+            border-bottom: variables.$navbar-border solid transparent;
+            display: inline-block;
+            line-height: 8rem;
+            &.active{
                 border-bottom: 0.5rem solid variables.$brand-color;
                 color: variables.$brand-color;
             }
